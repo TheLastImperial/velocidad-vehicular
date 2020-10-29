8b304fd904246734088e116e58fdf5356d728129
TheLastImperial <imperial435@gmail.com>
2020-10-29 12:04:47 -0700

    Trim trailing white space in tracker.py

--- TLI/tracker.py
+++ TLI/tracker.py
@@ -15,18 +15,18 @@ from TLI import utils
 class Tracker(): # class for Kalman Filter-based tracker
     def __init__(self):
         # Initialize parametes for tracker (history)
-        self.id = 0  # tracker's id 
-        self.box = [] # list to store the coordinates for a bounding box 
+        self.id = 0  # tracker's id
+        self.box = [] # list to store the coordinates for a bounding box
         self.hits = 0 # number of detection matches
         self.no_losses = 0 # number of unmatched tracks (track loss)
-        
+
         # Initialize parameters for Kalman Filtering
         # The state is the (x, y) coordinates of the detection box
         # state: [up, up_dot, left, left_dot, down, down_dot, right, right_dot]
         # or[up, up_dot, left, left_dot, height, height_dot, width, width_dot]
-        self.x_state=[] 
+        self.x_state=[]
         self.dt = 1.   # time interval
-        
+
         # Process matrix, assuming constant velocity model
         self.F = np.array([[1, self.dt, 0,  0,  0,  0,  0, 0],
                            [0, 1,  0,  0,  0,  0,  0, 0],
@@ -36,40 +36,40 @@ class Tracker(): # class for Kalman Filter-based tracker
                            [0, 0,  0,  0,  0,  1,  0, 0],
                            [0, 0,  0,  0,  0,  0,  1, self.dt],
                            [0, 0,  0,  0,  0,  0,  0,  1]])
-        
+
         # Measurement matrix, assuming we can only measure the coordinates
-        
+
         self.H = np.array([[1, 0, 0, 0, 0, 0, 0, 0],
                            [0, 0, 1, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 0, 1, 0, 0, 0], 
+                           [0, 0, 0, 0, 1, 0, 0, 0],
                            [0, 0, 0, 0, 0, 0, 1, 0]])
-        
-        
+
+
         # Initialize the state covariance
         self.L = 10.0
         self.P = np.diag(self.L*np.ones(8))
-        
-        
+
+
         # Initialize the process covariance
         self.Q_comp_mat = np.array([[self.dt**4/4., self.dt**3/2.],
                                     [self.dt**3/2., self.dt**2]])
-        self.Q = block_diag(self.Q_comp_mat, self.Q_comp_mat, 
+        self.Q = block_diag(self.Q_comp_mat, self.Q_comp_mat,
                             self.Q_comp_mat, self.Q_comp_mat)
-        
+
         # Initialize the measurement covariance
         self.R_scaler = 1.0
         self.R_diag_array = self.R_scaler * np.array([self.L, self.L, self.L, self.L])
         self.R = np.diag(self.R_diag_array)
-        
-        
-    def update_R(self):   
+
+
+    def update_R(self):
         R_diag_array = self.R_scaler * np.array([self.L, self.L, self.L, self.L])
         self.R = np.diag(R_diag_array)
-        
-        
-        
-        
-    def kalman_filter(self, z): 
+
+
+
+
+    def kalman_filter(self, z):
         '''
         Implement the Kalman Filter, including the predict and the update stages,
         with the measurement z
@@ -85,12 +85,12 @@ class Tracker(): # class for Kalman Filter-based tracker
         y = z - dot(self.H, x) # residual
         x += dot(K, y)
         self.P = self.P - dot(K, self.H).dot(self.P)
-        self.x_state = x.astype(int) # convert to integer coordinates 
+        self.x_state = x.astype(int) # convert to integer coordinates
                                      #(pixel values)
-        
-    def predict_only(self):  
+
+    def predict_only(self):
         '''
-        Implment only the predict stage. This is used for unmatched detections and 
+        Implment only the predict stage. This is used for unmatched detections and
         unmatched tracks
         '''
         x = self.x_state
@@ -103,20 +103,20 @@ def assign_detections_to_trackers(trackers, detections, iou_thrd = 0.3):
     '''
     From current list of trackers and new detections, output matched detections,
     unmatchted trackers, unmatched detections.
-    '''    
-    
+    '''
+
     IOU_mat= np.zeros((len(trackers),len(detections)),dtype=np.float32)
     for t,trk in enumerate(trackers):
-        #trk = convert_to_cv2bbox(trk) 
+        #trk = convert_to_cv2bbox(trk)
         for d,det in enumerate(detections):
          #   det = convert_to_cv2bbox(det)
             IOU_mat[t,d] = helpers.box_iou2(trk, det)
-    
-    # Produces matches       
+
+    # Produces matches
     # Solve the maximizing the sum of IOU assignment problem using the
     # Hungarian algorithm (also known as Munkres algorithm)
-    
-    indices = linear_sum_assignment(-IOU_mat)  
+
+    indices = linear_sum_assignment(-IOU_mat)
     indices = np.asarray(indices)
     matched_idx = np.transpose(indices)
 
@@ -130,48 +130,48 @@ def assign_detections_to_trackers(trackers, detections, iou_thrd = 0.3):
             unmatched_detections.append(d)
 
     matches = []
-   
-    # For creating trackers we consider any detection with an 
-    # overlap less than iou_thrd to signifiy the existence of 
+
+    # For creating trackers we consider any detection with an
+    # overlap less than iou_thrd to signifiy the existence of
     # an untracked object
-    
+
     for m in matched_idx:
         if(IOU_mat[m[0],m[1]]<iou_thrd):
             unmatched_trackers.append(m[0])
             unmatched_detections.append(m[1])
         else:
             matches.append(m.reshape(1,2))
-    
+
     if(len(matches)==0):
         matches = np.empty((0,2),dtype=int)
     else:
         matches = np.concatenate(matches,axis=0)
-    
-    return matches, np.array(unmatched_detections), np.array(unmatched_trackers)       
-    
+
+    return matches, np.array(unmatched_detections), np.array(unmatched_trackers)
+
 def pipeline(img, yolo, frame_count, max_age, min_hits, tracker_list,
     track_id_list):
     '''
     Pipeline function for detection and tracking
     '''
     frame_count+=1
-    
+
     img_dim = (img.shape[1], img.shape[0])
     coors, scores, classes = utils.get_bboxes(yolo, img)
     z_box = coors
     print("Caantidad: {}".format(str(len(classes))))
     if len(z_box) > 0:
         return img
-       
+
     x_box =[]
-    
+
     if len(tracker_list) > 0:
         for trk in tracker_list:
             x_box.append(trk.box)
 
     matched, unmatched_dets, unmatched_trks = assign_detections_to_trackers(x_box, z_box, iou_thrd = 0.3)
 
-    # Deal with matched detections     
+    # Deal with matched detections
     if matched.size >0:
         for trk_idx, det_idx in matched:
             z = z_box[det_idx]
@@ -184,8 +184,8 @@ def pipeline(img, yolo, frame_count, max_age, min_hits, tracker_list,
             tmp_trk.box =xx
             tmp_trk.hits += 1
             tmp_trk.no_losses = 0
-    
-    # Deal with unmatched detections      
+
+    # Deal with unmatched detections
     if len(unmatched_dets)>0:
         for idx in unmatched_dets:
             z = z_box[idx]
@@ -201,8 +201,8 @@ def pipeline(img, yolo, frame_count, max_age, min_hits, tracker_list,
             tmp_trk.id = track_id_list.popleft() # assign an ID for the tracker
             tracker_list.append(tmp_trk)
             x_box.append(xx)
-    
-    # Deal with unmatched tracks       
+
+    # Deal with unmatched tracks
     if len(unmatched_trks)>0:
         for trk_idx in unmatched_trks:
             tmp_trk = tracker_list[trk_idx]
@@ -213,22 +213,22 @@ def pipeline(img, yolo, frame_count, max_age, min_hits, tracker_list,
             xx =[xx[0], xx[2], xx[4], xx[6]]
             tmp_trk.box =xx
             x_box[trk_idx] = xx
-                   
-       
-    # The list of tracks to be annotated  
+
+
+    # The list of tracks to be annotated
     good_tracker_list =[]
     for trk in tracker_list:
         if ((trk.hits >= min_hits) and (trk.no_losses <=max_age)):
              good_tracker_list.append(trk)
              x_cv2 = trk.box
-             img = utils.draw_box_label(img, x_cv2) # Draw the bounding boxes on the 
+             img = utils.draw_box_label(img, x_cv2) # Draw the bounding boxes on the
                                              # images
     # Book keeping
-    deleted_tracks = filter(lambda x: x.no_losses >max_age, tracker_list)  
-    
+    deleted_tracks = filter(lambda x: x.no_losses >max_age, tracker_list)
+
     for trk in deleted_tracks:
             track_id_list.append(trk.id)
-    
+
     tracker_list = [x for x in tracker_list if x.no_losses<=max_age]
 
     return img
